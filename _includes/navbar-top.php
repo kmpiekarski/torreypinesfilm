99c126359e2f88e381cecfa096d4c09df37b6993
slashedtires <kenneth.m.piekarski@gmail.com>
2016-08-21 23:36:03 -0700

    Gave nav two columns, added a no-gutter class for columns (mean for elements with .row), provided space for mailchimp form

--- _includes/navbar-top.php
+++ _includes/navbar-top.php
@@ -11,18 +11,28 @@
         <span class="icon-bar"></span>
       </button>
     </div>
-    <div class="mobile-hide">
+    <div class="mobile-menu mobile-hide">
       <div class="navbar-collapse collapse bottom-collapse"> <!-- NOTE! The extra bottom-collapse class put on here -->
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="/trailer">Trailer</a></li>
-          <li><a href="/screenings">Screenings</a></li>
-          <li><a href="/donate">Donate</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-          <li><a href="https://www.facebook.com/torreypinesfilm" target="_new"><div class="circle-icon social-icons"><img src="/img/facebook-circle-icon.png"></div>
-          </a></li>
-          <li><a href="https://twitter.com/torreypinesfilm" target="_new"><div class="circle-icon social-icons"><img src="/img/twitter-circle-icon.png"></div></a></li>
-        </ul>
+        <div class="row no-gutter">
+          <div class="col-xs-6">
+            <ul class="nav navbar-nav">
+              <li><a href="/trailer">Trailer</a></li>
+              <li><a href="/screenings">Screenings</a></li>
+              <li><a href="/donate">Donate</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/contact">Contact</a></li>
+            </ul>
+          </div>
+          <div class="col-xs-6">
+            <ul class="nav navbar-nav">
+              <li><a href="https://www.facebook.com/torreypinesfilm" target="_new"><div class="circle-icon social-icons"><img src="/img/facebook-circle-icon.png"></div>
+              </a> <a href="https://twitter.com/torreypinesfilm" target="_new"><div class="circle-icon social-icons"><img src="/img/twitter-circle-icon.png"></div></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="row no-gutter">
+          mailchimp form goes here
+        </div>
       </div><!--/.nav-collapse -->
     </div>
   </div>
